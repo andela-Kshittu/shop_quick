0ad5dbb5008681e3332e7d4f75abca312e13ba36
Kenny Shittu <she2_adex2yahoo.com>
2014-09-11 13:30:22 +0100

    Final commit, responsive, dry and Obl pattern used

--- js/myScript.js
+++ js/myScript.js
@@ -4,21 +4,23 @@ var actions ={
 init: function(){
 actions.addtoList();
 actions.del();
+actions.edit();
 actions.moveItem($('.firstList'),$('.secondList'));
 actions.moveItem($('.secondList'),$('.firstList'));
 },
 // FETCH USER INPUT
 getUserItem: function(){
-	userItem = $('#userItem').val();
+	userItem = $.trim($('#userItem').val());
 },
 // INPUT VALIDATION METHOD
 validation: function(){
-	if (( userItem.length < 2)||(userItem.length > 30)){
-		alert("invalid description");
+
+	if (( userItem.length < 2)||(userItem.length > 25)){
+		alert("invalid description. \ndescription should not be more than 25 characters or less than two characters");
 	}
 	else 
 	{
-		$('.firstList').append("<li>"+ '<input type = "checkbox" class = "list" name ="list">'+userItem+'<button type = "delete" id = "delete">'+"Delete"+"</button>"+"</li>");
+		$('.firstList').append("<li>"+ '<input type = "checkbox" class = "list" name ="list">'+"<span>"+userItem+"</span>"+'<img src ="img/del.png" alt="" id ="delete">'+'<img src ="img/edit.png" alt="" id="edit">'+"</li>");
 	}
 },
 // ADD ITEMS TO LIST
@@ -34,6 +36,13 @@ addtoList: function(){
 // DELETE ITEMS FROM LIST
 del: function(){
 	$(document).on('click','#delete',function(){
+		$(this).parent().remove().hide("slow","ease-out");
+	});
+},
+edit: function(){
+	$(document).on('click','#edit',function(){
+		var value = $(this).parent().find($('span')).text();
+		$('#userItem').val(value);
 		$(this).parent().remove();
 	});
 },
