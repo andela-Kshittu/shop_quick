68fcc3c72c097c14ff1f08773eaf0b973849e596
Kenny Shittu <she2_adex2yahoo.com>
2014-09-12 22:00:45 +0100

    Favicon.ico added,index,js and css file editted

--- js/myScript.js
+++ js/myScript.js
@@ -1,15 +1,15 @@
 var userItem;
-// var allow;
+var allow=true;
 // THIS OBJECT HOLDS ALL METHODS
-var count1 = $('.firstList').length;
-var count2 = $('.secondList').length;
 var actions ={
 init: function(){
+	$('#mainhead').show(1700);
 actions.addtoList();
 actions.del();
 actions.edit();
 actions.moveItem($('.firstList'),$('.secondList'));
 actions.moveItem($('.secondList'),$('.firstList'));
+actions.hideitems();
 },
 // FETCH USER INPUT
 getUserItem: function(){
@@ -17,16 +17,13 @@ getUserItem: function(){
 },
 // INPUT VALIDATION METHOD
 validation: function(){
-
 	if (( userItem.length < 2)||(userItem.length > 25)){
 		alert("invalid description. \ndescription should not be more than 25 characters or less than two characters");
 	}
 	else 
 	{
-		$('.firstList').append("<li>"+ '<input type = "checkbox" class = "list" name ="list">'+"<span>"+userItem+"</span>"+'<input type="text" name="hiddenField" id="hiddentag"/>'+'<img src ="img/del.png" alt="" id ="delete">'+'<img src ="img/edit.png" alt="" id="edit">'+"</li>");
-
+		$('.firstList').append("<li>"+ '<input type = "checkbox" class = "list" name ="list">'+"<span>"+userItem+"</span>"+'<input type="text" name="hiddenField" id="hiddentag"/>'+'<img src ="img/del1.png" alt="" id ="delete">'+'<img src ="img/edit.png" alt="" id="edit">'+"</li>");
 	}
-
 },
 // ADD ITEMS TO LIST
 addtoList: function(){
@@ -37,7 +34,6 @@ addtoList: function(){
 // SET TEXT BOX TO EMPTY 
 	$('#userItem').val("");
     $('li input[type="text"]').hide();
-	// allow = true;
 });
 },
 // DELETE ITEMS FROM LIST
@@ -46,57 +42,51 @@ del: function(){
 		$(this).parent().hide("slow",function(){$(this).remove();});
 	});
 },
-// edit: function(){
-// 	$(document).on('click','#edit',function(){
-// 		if (allow === true){
-// 		var value = $(this).parent().find($('span')).text();
-// 		$('#userItem').val(value);
-// 		$(this).parent().remove();
-// 		$('ol').attr('disabled','disabled');
-// 		allow = false;
-// 	}
-// 		else{return false;}
-// 	});
-// },
-
+// EDIT ITEMS ON LIST
 edit: function(){
 	$(document).on('click','#edit',function(){
 	$(this).parent().find($('span')).hide('slow');
 	var value = $(this).parent().find($('span')).text();
-	$(this).parent().find($('input[type="text"]')).val(value);
-	$(this).parent().find($('input[type="text"]')).show('slow');
-	$(this).parent().find($('input[type="text"]')).focus();
-	$(this).parent().find($('input[type="text"]')).blur(function(){
-		$.trim($(this).parent().find($('input[type="text"]')).val());
-			if ($(this).parent().find($('input[type="text"]')).val()!== null){
-				// console.log($('input[type="text"]').val());
-			   $(this).parent().find($('span')).html( $(this).parent().find($('input[type="text"]')).val()).show(); 
-			   $(this).hide('slow');
-			// $(this).parent().find($('span'));
-
+	var hiddentag = $(this).parent().find($('input[type="text"]'));
+	hiddentag.val(value);
+	hiddentag.show('slow');
+	$(this).hide('slow')
+	hiddentag.focus();
+	hiddentag.blur(function(){
+		$(this).siblings($('#edit')).show('slow');
+		var valid = $.trim(hiddentag.val()).length;
+			if ((valid !== 0)&&(valid<26)){
+			   $(this).parent().find($('span')).html( hiddentag.val()).show(); 
+			   $(this).hide('slow');   
 		    }
+		    else{
+		 $(this).parent().find($('span')).html( value).show('slow'); 
+		 $(this).hide('slow');
+		 $(this).parent().find($('#edit')).show('slow');
+		 alert('Unable to edit, description is not valid\ndescription should not be more than 25 characters or less than 2 characters');
+			}
 	});
 });
 },
-// $('#hiddentag').val()
-// // editvalue: function(){
-// // 	$(this).parent().find($('input[type="text"]')).blur(function(){
-// // 			if ($(this).parent().find($('input[type="text"]')).val()!== ""){
-// //    console.log( $(this).parent().find($('span')).text($('#hiddentag').val()) );
-// //  	}
-// // 	$(this).remove().hide('slow');
-// // 	$(this).parent().find($('span')).show();
-// // });
-// },
 // MOVE ITEMS FROM BETWEEN TWO LISTS
 moveItem: function(list1,list2){
 	list1.on('change','input[type=checkbox]',function(){
 		list2.append($(this).parent());
-
 	});
-}};
+},
+hideitems:function(){
+	$('.hideicon').click(function(event){
+		event.preventDefault();
+		if (allow===true)
+		{
+	$(this).siblings('ol').hide('slow');
+		allow = false;
+	}
+		else{
+		$(this).siblings('ol').show('slow');
+		allow = true;	
+}
+});}
+};
 actions.init();
-	console.log(count1);
-	console.log(count2);
-
 
